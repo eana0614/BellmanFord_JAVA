13246eb3b948dccaae3bd9664f5e0f2e0e632642
eana0614 <eana0614@DESKTOP-C7AKB6O>
2017-04-23 21:14:26 +0900

    Realize Bellman Ford Algorithm.

--- BellmanFord_JAVA/src/bellman/App.java
+++ BellmanFord_JAVA/src/bellman/App.java
@@ -1,9 +1,27 @@
 package bellman;
 
 public class App {
+	
+
 
 	public static void main(String[] args) {
 		
+		int[][] weight = {{0, 6, 0, 7, 0}, 
+						  {0, 0, 5, 8, -4},
+						  {0, -2, 0, 0, 0},
+						  {0, 0, -3, 9, 0},
+						  {2, 0, 7, 0, 0}};
+		
+		BellmanFord bf = new BellmanFord();
+		
+		bf.createEdgesArray(weight);
+		bf.printEdges();
+		
+		boolean workable = bf.bellmanFord(0);
+		
+		if(workable){
+			bf.printDistance(0);
+		}
 		
 		
 	}
--- BellmanFord_JAVA/src/bellman/BellmanFord.java
+++ BellmanFord_JAVA/src/bellman/BellmanFord.java
@@ -1,5 +1,82 @@
 package bellman;
 
 public class BellmanFord {
+	
+	Edge[] edges;
+	int[] distance;
+	int INFINITY = Integer.MAX_VALUE;
+	
+	public void createEdgesArray(int[][] weight){
+		int vertexNum = weight.length;
+		int count = 0;
+		
+		Edge[] tempEdge = new Edge[vertexNum*vertexNum];
+		
+		for(int i=0; i < vertexNum; i++){
+			for(int j=0; j<vertexNum; j++){
+				if(weight[i][j] != 0){
+					tempEdge[count] = new Edge();
+					tempEdge[count].startVertex = i;
+					tempEdge[count].endVertex = j;
+					tempEdge[count].weight = weight[i][j];
+					count++;
+				}
+			}
+		}
+		
+		edges = new Edge[count];
+		for(int i=0; i<count; i++){
+			edges[i] = tempEdge[i];
+		}
+		
+		distance = new int[vertexNum];
+	}
 
+	public void printEdges(){
+		System.out.println("!] Print Edges.");
+		
+		for(int i=0; i<edges.length; i++){
+			System.out.println(i+"th Edge = start Vertex("+edges[i].startVertex+"), end Vertex("+edges[i].endVertex+")"
+								+"), weight("+edges[i].weight+")");
+		}
+	}
+	
+	public boolean bellmanFord(int start){
+
+		for(int i=0; i<distance.length; i++){
+			distance[i] = INFINITY;
+		}
+		
+		distance[start] = 0;
+		
+		for(int i=0; i<distance.length; i++){
+			for(int j=0; j<edges.length; j++){
+				relax(edges[j].startVertex, edges[j].endVertex, edges[j].weight);
+			}
+		}
+		
+		for(int i=0; i<edges.length; i++){
+			if(distance[edges[i].endVertex] > distance[edges[i].startVertex]+edges[i].weight){
+				return false;
+			}
+		}
+		
+		return true;
+	}
+
+	private void relax(int startVertex, int endVertex, int weight) {
+		
+		if(distance[endVertex] > distance[startVertex]+weight){
+			distance[endVertex] = distance[startVertex]+weight;
+		}
+	}
+
+	
+	public void printDistance(int start) {
+		
+		System.out.println("\n!] Print Distance. ");
+		for(int i=0; i<distance.length; i++){
+			System.out.println(start+"->"+i+" = "+distance[i]);
+		}
+	}
 }
